9e0f80a0d2eb9dfb661a0063c6451484e52ee0a6
noodles <noodles_loves@yahoo.com.tw>
2015-12-14 01:06:49 +0800

    自動判斷字元

--- static/show.js
+++ static/show.js
@@ -1,16 +1,26 @@
-function Go(){
+$(function(){
+  $('#input').keyup(function(){
   var input = $('#input').val();
-  $.ajax({
-         url:"/"+input,
-         crossDomain: true,
-         type:"GET",
-         dataType:'json',
-         success: function(html) {
-            result = html['result'];
-            $('#show').html("結果: "+result);
-         },
-        error:function(html){
-            alert('error');
-        }
-   });
-}
+  //len = input.length;
+    if(input != ""){
+      $('#show').html("");
+      $.ajax({
+             url:"/"+input,
+             crossDomain: true,
+             type:"GET",
+             dataType:'json',
+             success: function(html) {
+                result = html['result'];
+                result_split = result.split(",");
+                console.log(result_split);
+                $.each(result_split,function(k,v){
+          				$('#show').append("<div>"+v+"</div>"+"<br>").addClass("item");
+          			});
+             },
+            error:function(html){
+                alert('error');
+            }
+       });
+    }
+  });
+});
