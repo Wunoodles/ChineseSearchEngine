db51bc36bb6835e9c94d2a65155d261327e04338
Noodles <>
2015-12-14 14:38:12 +0800

    edit show

--- static/show.js
+++ static/show.js
@@ -3,24 +3,27 @@ $(function(){
   var input = $('#input').val();
   //len = input.length;
     if(input != ""){
-      $('#show').html("");
       $.ajax({
              url:"/"+input,
              crossDomain: true,
              type:"GET",
              dataType:'json',
              success: function(html) {
+                $('#show').html("");
                 result = html['result'];
                 result_split = result.split(",");
                 console.log(result_split);
                 $.each(result_split,function(k,v){
-          				$('#show').append("<div>"+v+"</div>"+"<br>").addClass("item");
+          				$('#show').append("<div class=\' item \'>"+v+"</div>");
           			});
              },
             error:function(html){
                 alert('error');
             }
        });
+    }
+    if(input == ""){
+      $('#show').html("");
     }
   });
 });
