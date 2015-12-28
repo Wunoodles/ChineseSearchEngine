115f7b48a79af9bff4abeec12cd381c4ec106b3c
noodles <noodles_loves@yahoo.com.tw>
2015-12-28 23:44:41 +0800

    key_lenght 1 show

--- static/show.js
+++ static/show.js
@@ -2,27 +2,42 @@ $(function(){
   $('#input').keyup(function(){
   var input = $('#input').val();
   //len = input.length;
-    if(input != ""){
+	//var keynum = (event.keyCode ? event.keyCode : event.which);
+	//alert(keynum);
+	if(input == 'N'){
+		$('#show').append("沒有匹對");
+	}
+    if(input != "" && input != 'NULL'){
       $.ajax({
              url:"/"+input,
              crossDomain: true,
              type:"GET",
              dataType:'json',
              success: function(html) {
-                $('#show').html("");
+				$('#show').html("");
                 result = html['result'];
-                result_split = result.split(",");
-                console.log(result_split);
-                $.each(result_split,function(k,v){
-          				$('#show').append("<div class=\' item \'>"+v+"</div>");
+				key = html['input'];
+				collocation = html['collocation'];
+				console.log(result);
+				
+				$.each(result,function(k,v){
+					$('#show').append("<div class='item'>"+key+"+"+v+"</div>");
+          		});
+				
+				collocation_split = collocation.split(",");
+                console.log(collocation_split);
+				
+                $.each(collocation_split,function(k,v){
+          				$('#show').append("<div class='item2'>"+key+"+"+v+"</div>");
           			});
+				
              },
             error:function(html){
                 alert('error');
             }
        });
     }
-    if(input == ""){
+	if(input == ""){
       $('#show').html("");
     }
   });
